8622fa956d03d80c65c09652fb2197a36e8af154
Charles Averill <charlesaverill20@gmail.com>
2025-09-06 14:49:54 -0500

    Make window resizable, sacrifice auto resizing of window for small images

--- lib/app/render.ml
+++ lib/app/render.ml
@@ -1,6 +1,5 @@
 open Tsdl
 open Background
-open Init
 open Formats.Format
 
 type settings = {mutable scale: float; mutable offset: int * int}
@@ -14,30 +13,22 @@ let clear window renderer =
   Sdl.render_clear renderer |> ignore ;
   draw_checker_background renderer (get_checker_texture renderer 16) win_w win_h
 
-let compute_initial_window_and_scale (img : format) =
-  (* Determine scaling factor to fit image in max window size *)
-  let scale_w = float !max_win_w /. float img#width in
-  let scale_h = float !max_win_h /. float img#height in
-  let scale = min 1.0 (min scale_w scale_h) in
-  (* never upscale *)
-  (* Compute window size for initial render *)
-  let win_w = int_of_float (float img#width *. scale) in
-  let win_h = int_of_float (float img#height *. scale) in
-  (win_w, win_h, scale)
+let compute_fit_scale (img : format) (win_w, win_h) =
+  (* Compute scaling factor to fit image within window *)
+  let scale_w = float win_w /. float img#width in
+  let scale_h = float win_h /. float img#height in
+  (* don’t upscale, only shrink *)
+  min 1.0 (min scale_w scale_h)
 
 let draw_texture window renderer (img : Formats.Format.format)
     (texture : Sdl.texture option) (settings : settings) =
-  (* On first render, compute scale and window size if scale = 1 *)
+  (* If first render (scale = 1.0), compute fit-to-window scale *)
   if settings.scale = 1.0 then (
-    let win_w, win_h, scale = compute_initial_window_and_scale img in
+    let win_w, win_h = Sdl.get_window_size window in
+    let scale = compute_fit_scale img (win_w, win_h) in
     settings.scale <- scale ;
-    Printf.printf "%s %f\n" img#filename settings.scale ;
-    flush stdout ;
-    (* if scale < 1.0 then *)
-    (* Sdl.render_set_logical_size renderer win_w win_h |> ignore ; *)
-    Sdl.set_window_size window ~w:win_w ~h:win_h |> ignore ;
-    Init.owin_w := win_w ;
-    Init.owin_h := win_h
+    Printf.printf "%s scale=%.3f\n" img#filename settings.scale ;
+    flush stdout
   ) ;
   let scaled_w = int_of_float (float img#width *. settings.scale) in
   let scaled_h = int_of_float (float img#height *. settings.scale) in
